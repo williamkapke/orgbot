01dc839064c6cbcc9dfcdc047261e4cf5c356aa4
William <william.kapke@gmail.com>
2016-04-04 05:04:22 +0000

    use emit override

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 var http = require('http');
+var port = process.env.PORT || 9207;
+
 var events = require('github-webhook-handler')({
   path: '/github',
-  secret: process.env.WEBHOOK_SECRET
+  secret: process.env.WEBHOOK_SECRET,
+  emit: (headers, data)=>{
+    data.action = (data.action+'.'||'') + headers['x-github-event'];
+    console.log(JSON.stringify(data, null, 2));
+    handler.emit(data.action, data);
+  }
 });
-var port = process.env.PORT || 9207;
 
 http.createServer((req, res) =>
   events(req, res, _=> {
@@ -16,21 +22,6 @@ http.createServer((req, res) =>
 events.on('error', err =>
   console.error('Error:', err.message)
 );
-
-events.on('*', event =>{
-  //make this more semantic
-  var name = event.event;
-  var payload = event.payload;
-  Object.keys(event).forEach(key=>delete event[key]);
-  event.name = name;
-  Object.keys(payload).forEach(key=>event[key] = payload[key]);
-
-  console.log(JSON.stringify(event, null, 2));
-
-  //allow more granularity
-  if(event.action)
-    events.emit(event.name +'.'+ event.action, event);
-});
 
 require('fs').readdirSync('./scripts').forEach(file=>{
   file = './scripts/' + file;
